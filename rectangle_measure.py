4b264a535086d1a184f77919585ea610ea07d49e
Jose Arroyo <1047719@MY.SCCCD.EDU>
2026-08-12 13:30:57 -0700

    Added Missing Programs

---
+++ rectangle_measure.py
@@ -0,0 +1,8 @@
+width = 17
+height = 12.0
+
+floor_divide = width // 2
+divide_1 = width / 2.0
+divide_2 = height / 3
+total = height + width * 5
+print("The overall area of the rectangle is:", total)
